ec765917829f42f1330ee79fc7b555a831a8da4a
gabe <gabrieltucker260@gmail.com>
2023-10-31 14:23:38 -0700

    added sty;les
    Co-authored-by: Grant Abel <gtabel88@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,14 @@
 // Test 3: Make a deposit to the existing bank account.
 // Test 4: Make a withdrawal from the existing bank account.
 // Business Logic
-function BankAccount(firstName, lastName, balance) {
+function BankAccount(firstName, lastName, balance, email) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.balance = balance;
+    this.email = email;
 }
+
+const account = new BankAccount(firstNameInput, lastNameInput, balanceInput);
 
 BankAccount.prototype.deposit = function(amount) {
     this.balance += amount;
@@ -17,30 +20,35 @@ BankAccount.prototype.withdraw = function(amount) {
     if (amount <= this.balance) {
         this.balance -= amount;
     } else {
-        console.log("Insufficient balance");
+        this.balance = "Account balance insufficient. Please enter new amount."
     }
 };
 
 // UI Logic
 function formHandler(event) {
     event.preventDefault();
+    document.getElementById("Result").setAttribute("class", "hidden");
     const firstNameInput = document.querySelector("input[name='firstName']").value;
     const lastNameInput = document.querySelector("input[name='lastName']").value;
+    const EmailInput = document.querySelector("input[name='inputEmail']").value;
     const balanceInput = parseFloat(document.querySelector("input[name='inputBalance']").value);
     const depositInput = parseFloat(document.querySelector("input[name='inputDeposit']").value);
     const withdrawalInput = parseFloat(document.querySelector("input[name='inputwithdrawal']").value);
 
-    const account = new BankAccount(firstNameInput, lastNameInput, balanceInput);
+// Perform deposit and withdrawal
+account.deposit(depositInput);
+account.withdraw(withdrawalInput);
 
-    // Perform deposit and withdrawal
-    account.deposit(depositInput);
-    account.withdraw(withdrawalInput);
-
-    // Display the result
-    document.getElementById("result").textContent = `New Balance: $${account.balance.toFixed(2)}`;
+// Display the result
+document.getElementById("result").textContent = `New Balance: $${account.balance.toFixed(2)}`;
 }
 
 window.addEventListener("DOMContentLoaded", function () {
     const form = document.querySelector("#bankForm");
+    form.addEventListener("submit", formHandler);
+});
+
+window.addEventListener("onclick", function () {
+    const form = document.querySelector("#bankForm2");
     form.addEventListener("submit", formHandler);
 });
